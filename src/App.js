85376f436d82aaf92be9796d6f2ac24a538bc66d
Elias <eliasdamianlopez@gmail.com>
2020-05-06 21:07:09 -0500

    solucionado

--- src/App.js
+++ src/App.js
@@ -3,6 +3,29 @@ import React, { Component } from 'react';
 /// Modifica el componente para que se puedan agregar tareas
 
 class App extends Component {
+
+  state = {
+    todo: '',
+    list:[]
+  }
+  
+
+  updateTodo = (event) => {
+    this.setState({
+      todo: event.target.value
+    });
+  }
+
+  handleKeyPress = (event) => {
+    if(event.key === 'Enter'){
+      event.preventDefault();
+      this.setState((prevState) => ({
+        list: prevState.list.concat(prevState.todo),
+        todo:''
+      }));
+    }
+  }
+  
   render() {
     return (
       <div className="wrapper">
@@ -12,9 +35,18 @@ class App extends Component {
             <li>Sacar la ropa</li>
             <li>Hacer la cama</li>
             <li>Leer un rato</li>
+            {this.state.list.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
           </ul>
            <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
+             <input type="text" 
+                    id="new-task"
+                    value={this.state.todo}
+                    onChange={this.updateTodo} 
+                    onKeyPress={this.handleKeyPress} 
+                    placeholder="Ingresa una tarea y oprime Enter" 
+                    />
            </form>
         </div>
       </div>
